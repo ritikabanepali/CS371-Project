6c91d47faf3387df77345f0390f30c31697cbdb2
Ritika Banepali <ritikabanepali@gmail.com>
2025-07-01 16:30:38 -0500

    Initial Commit

---
+++ CS371-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS371-Project
+//
+//  Created by ritika banepali on 7/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
