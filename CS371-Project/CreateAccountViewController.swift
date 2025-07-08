e3b3813c40632f1842180073a7f1a9cb2f29b3d3
suhani <suhanigos7@gmail.com>
2025-07-08 14:58:47 -0500

    First push

---
+++ CS371-Project/CreateAccountViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateAccountViewController.swift
+//  CS371-Project
+//
+//  Created by Suhani Goswami on 7/8/25.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func joinButton(_ sender: Any) {
+        
+    }
+}
