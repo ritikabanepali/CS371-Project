3820ba1c01c8fd35e662d71fd91c44bd53a3262f
Ritika Banepali <ritikabanepali@gmail.com>
2025-07-08 15:16:10 -0500

    Ritika's first commit

---
+++ CS371-Project/LocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationViewController.swift
+//  CS371-Project
+//
+//  Created by ritika banepali on 7/6/25.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CS371-Project/TravelerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelerViewController.swift
+//  CS371-Project
+//
+//  Created by ritika banepali on 7/5/25.
+//
+
+import UIKit
+
+class TravelerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.tintColor = .black
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
