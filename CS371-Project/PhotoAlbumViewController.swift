2a8c9747d1dd58f5b58509446ae20ac06e0527ff
Julia Mergel <jm.mergel@gmail.com>
2025-07-11 14:31:13 -0500

    photo album test

--- CS371-Project/PhotoAlbumViewController.swift
+++ CS371-Project/PhotoAlbumViewController.swift
@@ -2,15 +2,45 @@
 //  PhotoAlbumViewController.swift
 //  CS371-Project
 //
-//  Created by Julia  on 7/9/25.
+//  Created by Julia on 7/9/25.
 //
 
 import UIKit
+import PhotosUI
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, PHPickerViewControllerDelegate {
+
+    // Connect this to your UIImageView in storyboard
+    // Connect this to your "Add Photo" button in storyboard (IBAction)
+    @IBOutlet weak var imageView: UIImageView!
     
+    @IBAction func addPhotosButtonTapped(_ sender: Any) {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
     override func viewDidLoad() {
-          super.viewDidLoad()
-          // setup code
-      }
+        super.viewDidLoad()
+        // Any setup if needed
+    }
+
+    // Delegate method
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+
+        guard let result = results.first else { return }
+
+        result.itemProvider.loadObject(ofClass: UIImage.self) { (object, error) in
+            if let image = object as? UIImage {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }
+    }
 }
